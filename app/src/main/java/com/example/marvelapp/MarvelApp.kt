b6a13c04b950229e88253817f5f093bd0153e23a
Bruno Lima <contato@blimas.dev>
2023-02-09 23:25:08 -0300

    Armazenando as chaves da API da Marvel de forma segura no projeto

--- app/src/main/java/com/example/marvelapp/MarvelApp.kt
+++ app/src/main/java/com/example/marvelapp/MarvelApp.kt
@@ -2,7 +2,5 @@ package com.example.marvelapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-
 @HiltAndroidApp
-class MarvelApp : Application() {
-}
+class MarvelApp : Application()
