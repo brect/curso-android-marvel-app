77d4205e8b0058618947370615d0430ce7f2155b
Bruno Lima <contato@blimas.dev>
2023-02-15 22:51:24 -0300

    Implementa implementa characters paging source

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,8 +1,17 @@
 package com.example.marvelapp.framework.network.response
 
-data class CharacterResponse (
+import com.example.core.domain.model.Character
+
+data class CharacterResponse(
     val id: String,
     val name: String,
-    val thumbnail: ThumbnailResponse
+    val thumbnail: ThumbnailResponse,
 
-        )
+    )
+
+fun CharacterResponse.toCharacterModel(): Character {
+    return Character(
+        name = this.name,
+        imageUrl = "${this.thumbnail.path}.${this.thumbnail.extension}"
+    )
+}
