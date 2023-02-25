8dc5be26fa59b0621e1fb2de1abd7c00e526e6e6
Bruno Lima <contato@blimas.dev>
2023-02-24 23:57:36 -0300

    Configura DI para server local e implementa teste instrumentado

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,15 +1,30 @@
 package com.example.marvelapp.presentation.characters
 
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.marvelapp.R
+import com.example.marvelapp.extension.asJsonString
+import com.example.marvelapp.framework.di.BaseUrlModule
 import com.example.marvelapp.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.Matcher
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@UninstallModules(BaseUrlModule::class)
 @HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class CharactersFragmentTest {
@@ -17,14 +32,40 @@ class CharactersFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    private lateinit var mockWebServer: MockWebServer
+
     @Before
-    fun setup(){
+    fun setup() {
+        mockWebServer = MockWebServer().apply {
+            start(8080)
+        }
         launchFragmentInHiltContainer<CharactersFragment>()
     }
 
     @Test
-    fun shouldShowCharacters_when_ViewIsCreated(){
+    fun shouldShowCharacters_when_ViewIsCreated() {
+        mockWebServer.enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
+        onView(
+            withId(R.id.recycler_characters)
+        ).check(
+            matches(isDisplayed())
+        )
+    }
 
+    @After
+    fun tearDown(){
+        mockWebServer.shutdown()
+    }
+
+    private fun waitFor(delay: Long): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> = isRoot()
+            override fun getDescription(): String = "wait for $delay milliseconds"
+            override fun perform(uiController: UiController, view: View?) {
+                uiController.loopMainThreadForAtLeast(delay)
+            }
+
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/marvelapp/framework/di/BaseUrlModule.kt
@@ -0,0 +1,16 @@
+package com.example.marvelapp.framework.di
+
+import com.example.marvelapp.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object BaseUrlModule {
+
+    @BaseUrl
+    @Provides
+    fun provideBaseUrl(): String = BuildConfig.BASE_URL
+}
