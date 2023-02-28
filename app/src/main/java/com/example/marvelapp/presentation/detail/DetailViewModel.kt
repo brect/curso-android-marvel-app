d473b2962e0acbce03fc0177608376cbf4704b31
Bruno Lima <contato@blimas.dev>
2023-02-28 00:25:45 -0300

    Implementa layout de detalhes e refatora implementacao de image loader

---
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.marvelapp.presentation.detail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+class DetailViewModel (): ViewModel()
