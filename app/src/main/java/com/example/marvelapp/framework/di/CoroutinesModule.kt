f8853a6ba9c98c78522f4f4fd0841311435add0a
Bruno Lima <contato@blimas.dev>
2023-03-30 22:15:25 -0300

    Implementando os testes unitários do view model do detalhe

--- app/src/main/java/com/example/marvelapp/framework/di/CoroutinesModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/CoroutinesModule.kt
@@ -2,6 +2,7 @@ package com.example.marvelapp.framework.di
 
 import com.example.core.usecase.base.AppCoroutinesDispatchers
 import com.example.core.usecase.base.CoroutinesDispatchers
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,7 +11,6 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 interface CoroutinesModule {
-
-    @Provides
+    @Binds
     fun provideDispatchers(dispatchers: AppCoroutinesDispatchers): CoroutinesDispatchers
 }
--- app/src/test/java/com/example/marvelapp/presentation/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/example/marvelapp/presentation/detail/DetailViewModelTest.kt
@@ -2,15 +2,26 @@ package com.example.marvelapp.presentation.detail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.example.core.domain.model.Comic
+import com.example.core.domain.model.Event
 import com.example.core.usecase.GetCharactersCategoryUseCase
+import com.example.core.usecase.base.ResultStatus
+import com.example.marvelapp.R
 import com.example.testing.MainCoroutineRule
 import com.example.testing.model.CharacterFactory
 import com.example.testing.model.ComicFactory
 import com.example.testing.model.EventFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.isA
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -39,11 +50,67 @@ class DetailViewModelTest {
     private val events = listOf(EventFactory().create(EventFactory.FakeEvent.FakeEvent1))
 
     @Before
-    fun setup(){
+    fun setup() {
         detailViewModel = DetailViewModel(useCase)
         detailViewModel.uiState.observeForever(uiStateObserver)
     }
 
+    @Test
+    fun `should notify uiState with Success from UiState when get character categories returns success`() {
+        runTest {
+            // Arrange
+            whenever(useCase.invoke(any())).thenReturn(
+                flowOf(
+                    ResultStatus.Success(
+                        comics to events
+                    )
+                )
+            )
 
+            // Act
+            detailViewModel.getCharacterCategory(character.id)
+
+
+            // Assert
+            verify(uiStateObserver)
+                .onChanged(
+                    isA<DetailViewModel.UiState.Success>()
+                )
+
+            val uiStateSuccess = detailViewModel.uiState.value as DetailViewModel.UiState.Success
+            val categoriesParentList = uiStateSuccess.detailParentList
+
+            assertEquals(2, categoriesParentList.size)
+            assertEquals(
+                R.string.details_comics_category,
+                categoriesParentList[0].categoryStringResId
+            )
+            assertEquals(
+                R.string.details_events_category,
+                categoriesParentList[1].categoryStringResId
+            )
+
+        }
+    }
+
+    @Test
+    fun `should notify uiState with Success from UiState when get character categories returns only comics`() {
+        // TODO: Implement tests
+    }
+
+    @Test
+    fun `should notify uiState with Success from UiState when get character categories returns only events`() {
+        // TODO: Implement tests
+    }
+
+    @Test
+    fun `should notify uiState with Empty from UiState when get character categories returns an empty result list`() {
+        // TODO: Implement tests
+    }
+
+    @Test
+    fun `should notify uiState with Error from UiState when get character categories returns an exception`() {
+        // TODO: Implement tests
+    }
 
 }
