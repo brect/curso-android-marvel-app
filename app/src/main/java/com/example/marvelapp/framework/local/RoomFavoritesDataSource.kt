65ebf8de107c8b97af0aa299fd2c65be46d3a7b6
Bruno Lima <contato@blimas.dev>
2023-04-10 23:25:54 -0300

    Implementando o repositório e as fontes de dados para favoritos

---
+++ app/src/main/java/com/example/marvelapp/framework/local/RoomFavoritesDataSource.kt
@@ -0,0 +1,31 @@
+package com.example.marvelapp.framework.local
+
+import com.example.core.data.repository.FavoritesLocalDataSource
+import com.example.core.domain.model.Character
+import com.example.marvelapp.framework.db.dao.FavoriteDAO
+import com.example.marvelapp.framework.db.entity.FavoriteEntity
+import com.example.marvelapp.framework.db.entity.toCharacterModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class RoomFavoritesDataSource @Inject constructor(
+    private val favoritesDAO: FavoriteDAO
+) : FavoritesLocalDataSource {
+
+    override suspend fun getAll(): Flow<List<Character>> {
+        return favoritesDAO.loadFavorites().map {
+            it.toCharacterModel()
+        }
+    }
+
+    override suspend fun saveFavorite(character: Character) {
+        favoritesDAO.insertFavorite(character.toFavoriteEntity())
+    }
+
+    override suspend fun deleteFavorite(character: Character) {
+        favoritesDAO.insertFavorite(character.toFavoriteEntity())
+    }
+
+    private fun Character.toFavoriteEntity() = FavoriteEntity(id, name, imageUrl)
+}
