2c54417549e1a608c48db64c520800fbb3041c20
Bruno Lima <contato@blimas.dev>
2023-04-22 20:58:26 -0300

    Implemento a entidade e o DAO para as chaves remotas

--- app/src/main/java/com/example/marvelapp/framework/db/AppDatabase.kt
+++ app/src/main/java/com/example/marvelapp/framework/db/AppDatabase.kt
@@ -2,12 +2,25 @@ package com.example.marvelapp.framework.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.marvelapp.framework.db.dao.CharacterDAO
 import com.example.marvelapp.framework.db.dao.FavoriteDAO
+import com.example.marvelapp.framework.db.entity.CharacterEntity
 import com.example.marvelapp.framework.db.entity.FavoriteEntity
+import com.example.marvelapp.framework.db.entity.RemoteKey
 
-@Database(entities = [FavoriteEntity::class], version = 1, exportSchema = false)
-abstract class AppDatabase: RoomDatabase() {
+@Database(
+    entities = [
+        FavoriteEntity::class,
+        CharacterEntity::class,
+        RemoteKey::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun favoriteDao(): FavoriteDAO
+    abstract fun characterDao(): CharacterDAO
+    abstract fun remoteKey(): RemoteKey
 
 }
