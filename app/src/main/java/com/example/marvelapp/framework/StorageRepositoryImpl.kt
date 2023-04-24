7a347806f47f55862a902ac446d7bc38e72ec8ce
Bruno Lima <contato@blimas.dev>
2023-04-23 22:24:55 -0300

    Implementando a fonte de dados local com data store

--- app/src/main/java/com/example/marvelapp/framework/StorageRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelapp/framework/StorageRepositoryImpl.kt
@@ -3,11 +3,12 @@ package com.example.marvelapp.framework
 import com.example.core.data.repository.StorageLocalDataSource
 import com.example.core.data.repository.StorageRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class StorageRepositoryImpl(
+class StorageRepositoryImpl @Inject constructor(
     private val storageLocalDataSource: StorageLocalDataSource,
-) :
-    StorageRepository {
+) : StorageRepository {
+
     override val sorting: Flow<String>
         get() = storageLocalDataSource.sorting
 
