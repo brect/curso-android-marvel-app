f5c6e43a60271d8620f2d2c80a7a724ae6ddf496
Bruno Lima <contato@blimas.dev>
2023-04-23 21:49:22 -0300

    Adicionando options menu na toolbar e navegando para a tela de ordenação

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.ActivityMainBinding
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbarApp)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
 
@@ -35,9 +38,12 @@ class MainActivity : AppCompatActivity() {
             setOf(
                 R.id.charactersFragment,
                 R.id.favoritesFragment,
-                R.id.aboutFragment
+                R.id.aboutFragment,
+                R.id.sortFragment
             )
         )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
 
