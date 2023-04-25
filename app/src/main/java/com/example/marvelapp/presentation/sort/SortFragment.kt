9db64c4ecb2d550963534646dda28984f6e617b2
Bruno Lima <contato@blimas.dev>
2023-04-24 23:27:03 -0300

    Implementando os listeners para o componente ChipGroup no fragmento

--- app/src/main/java/com/example/marvelapp/presentation/sort/SortFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/sort/SortFragment.kt
@@ -7,9 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.example.core.domain.model.SortingType
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.FragmentSortBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,13 +20,45 @@ class SortFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentSortBinding? = null
     private val binding: FragmentSortBinding get() = _binding!!
 
-    private val  viewModel: SortViewModel by viewModels()
+    private var orderBy = SortingType.ORDER_BY_NAME.value
+    private var order = SortingType.ORDER_BY_ASCENDING.value
+
+    private val viewModel: SortViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_sort, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setChipGroupListeners()
+    }
+
+    private fun setChipGroupListeners() {
+        binding.chipGroupOrderBy.setOnCheckedChangeListener { group, checkedId ->
+            val chip = group.findViewById<Chip>(checkedId)
+            orderBy = getOrderByValue(chip.id)
+        }
+
+        binding.chipGroupOrder.setOnCheckedChangeListener { group, checkedId ->
+            val chip = group.findViewById<Chip>(checkedId)
+            order = getOrderValue(chip.id)
+        }
+    }
+
+    private fun getOrderByValue(chipId: Int): String = when (chipId) {
+        R.id.chip_name -> SortingType.ORDER_BY_NAME.value
+        R.id.chip_modified -> SortingType.ORDER_BY_MODIFIED.value
+        else -> SortingType.ORDER_BY_NAME.value
+    }
+
+    private fun getOrderValue(chipId: Int): String = when (chipId) {
+        R.id.chip_ascending -> SortingType.ORDER_BY_ASCENDING.value
+        R.id.chip_descending -> SortingType.ORDER_BY_DESCENDING.value
+        else -> SortingType.ORDER_BY_ASCENDING.value
     }
 
     override fun onDestroyView() {
